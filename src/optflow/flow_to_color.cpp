7c5fc39dfba1606ff27596e489bee97615be0c46
Sanggyu Nam <pokeplus@gmail.com>
2015-02-10 22:57:44 +0900

    Fix flowToColor to work

--- src/optflow/flow_to_color.cpp
+++ src/optflow/flow_to_color.cpp
@@ -1,6 +1,8 @@
 #include "flow_to_color.hpp"
 #include "colorcode.h"
 
+#include <opencv2/opencv.hpp>
+
 #include <cmath>
 #include <limits>
 
@@ -13,51 +15,68 @@ using namespace opticalflow;
 
 void ugproj::flowToColor(MCImageDoubleX &vx, MCImageDoubleX &vy,
                          cv::Mat& img) {
+    typedef Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic> ArrayDXX;
+
     const int width = vx.width();
     const int height = vy.height();
-    
-    Eigen::Matrix2d u = vx;
-    Eigen::Matrix2d v = vy;
-    double maxU = INIT_VAL_MAX, maxV = INIT_VAL_MAX;
-    double minU = INIT_VAL_MIN, minV = INIT_VAL_MIN;
-    double maxRad = -1;
-    
-    // fix unknown flow
-    Eigen::ArrayXXi idxUnknown = ((u.array().abs() > UNKNOWN_FLOW_THRESHOLD) ||
-                                  (v.array().abs() > UNKNOWN_FLOW_THRESHOLD)).cast<int>();
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            if (idxUnknown.coeffRef(i, j) == 0)
-                u.coeffRef(i, j) = v.coeffRef(i, j) = 0;
+
+    // copy array
+    ArrayDXX u(height, width), v(height, width);
+    for (int c = 0; c < width; ++c) {
+        for (int r = 0; r < height; ++r) {
+            u.coeffRef(r, c) = vx(c, r, 0);
+            v.coeffRef(r, c) = vy(c, r, 0);
         }
     }
-    
+
+    // fix unknown flow
+    Eigen::Array<bool, Eigen::Dynamic, Eigen::Dynamic> idxUnknown(height, width);
+    idxUnknown = (u.abs() > UNKNOWN_FLOW_THRESHOLD) ||
+                 (v.abs() > UNKNOWN_FLOW_THRESHOLD);
+    for (int c = 0; c < height; ++c) {
+        for (int r = 0; r < width; ++r) {
+            if (idxUnknown.coeffRef(r, c))
+                u.coeffRef(r, c) = v.coeffRef(r, c) = 0;
+        }
+    }
+
+    // get mix/max value (only for debug)
+    double maxU = INIT_VAL_MAX, maxV = INIT_VAL_MAX;
+    double minU = INIT_VAL_MIN, minV = INIT_VAL_MIN;
     maxU = u.maxCoeff();
     minU = u.minCoeff();
-    
+
     maxV = v.maxCoeff();
     minV = v.minCoeff();
-    
-    Eigen::Matrix2d rad = (u.array().square() + v.array().square()).cwiseSqrt();
+
+    // get maximum norm
+    double maxRad = -1;
+    ArrayDXX rad = (u.square() + v.square()).sqrt();
     maxRad = rad.maxCoeff();
-    
+
+    // normalize vectors
     double eps = numeric_limits<double>::epsilon();
     u /= maxRad + eps;
     v /= maxRad + eps;
-    
+
+    // initialize flow image matrix
+    img.create(height, width, CV_8UC3);
+
+    // fill flow image
     cv::Scalar color;
+    cv::Point point;
     uchar pix[3];
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            if (idxUnknown.coeffRef(i, j) == 0) {
+    for (int c = 0; c < width; ++c) {
+        for (int r = 0; r < height; ++r) {
+            if (idxUnknown.coeffRef(r, c)) {
                 // unknown flow
                 color = CV_RGB(0, 0, 0);
             } else {
-                // compute color
-                computeColor(u.coeffRef(i, j), v.coeffRef(i, j), pix);
-                color = CV_RGB(pix[0], pix[1], pix[2]);
+                computeColor(u.coeffRef(r, c), v.coeffRef(r, c), pix);
+                color = CV_RGB(pix[2], pix[1], pix[0]);
             }
-            img.at<cv::Scalar>(i, j) = color;
+            point = cv::Point(c, r);
+            cv::line(img, point, point, color);
         }
     }
 }
