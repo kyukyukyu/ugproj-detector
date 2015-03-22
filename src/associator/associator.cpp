9be156b1366c60d9f9e6eca9b65f9201d5fb7bfb
Sanggyu Nam <pokeplus@gmail.com>
2015-03-23 00:07:22 +0900

    Indent using spaces instead of tab
    * Please, please, please!!

--- src/associator/associator.cpp
+++ src/associator/associator.cpp
@@ -135,7 +135,7 @@ void OpticalFlowFaceAssociator::calculateProb() {
 
 void SiftFaceAssociator::calculateProb() {
 
-	calculateNextRect();
+    calculateNextRect();
 
     int prevCddsSize, nextCddsSize;
     prevCddsSize = this->prevCandidates.size();
@@ -175,9 +175,9 @@ void SiftFaceAssociator::calculateProb() {
         const cv::KeyPoint& kpB = keypointsB[indexB];
         vector<int> selectedIdxsA = vector<int>();
         vector<int> selectedIdxsB = vector<int>();
-	   for (int i = 0; i < prevCddsSize; ++i) {
+        for (int i = 0; i < prevCddsSize; ++i) {
             if (prevCandidates[i]->rect.contains(kpA.pt)) {
-				selectedIdxsA.push_back(i);
+                selectedIdxsA.push_back(i);
             }
         }
 
@@ -222,238 +222,238 @@ void findSAB(Eigen::MatrixXd& matA, Eigen::VectorXd& matB, Eigen::VectorXd& matX
 
 void findSxSyAB(int N, int M, vector <double> &A, vector <double> &B, vector <double> &X)
 {
-	// find Sx, Sy, A, B by inverse
-	Eigen::MatrixXd _A(N, M);
-	Eigen::MatrixXd _B(N, 1);
-	Eigen::MatrixXd _X(M, 1);
+    // find Sx, Sy, A, B by inverse
+    Eigen::MatrixXd _A(N, M);
+    Eigen::MatrixXd _B(N, 1);
+    Eigen::MatrixXd _X(M, 1);
 
-	for (int i = 0; i < N; i++) {
-		_B(i, 0) = B[i];
-		for (int j = 0; j < M; j++) {
-			_A(i, j) = A[i*M + j];
-		}
-	}
-	_X = _A.inverse()*_B;
+    for (int i = 0; i < N; i++) {
+        _B(i, 0) = B[i];
+        for (int j = 0; j < M; j++) {
+            _A(i, j) = A[i*M + j];
+        }
+    }
+    _X = _A.inverse()*_B;
 
-	for (int i = 0; i < M; i++)
-		X[i] = _X(i, 0);
+    for (int i = 0; i < M; i++)
+        X[i] = _X(i, 0);
 }
 
 void SiftFaceAssociator::calculateNextRect() {
 
-	int prevCddsSize, nextCddsSize;
-	prevCddsSize = this->prevCandidates.size();
-	nextCddsSize = this->nextCandidates.size();
+    int prevCddsSize, nextCddsSize;
+    prevCddsSize = this->prevCandidates.size();
+    nextCddsSize = this->nextCandidates.size();
 
-	cv::Scalar colorPreset[] = {
-		CV_RGB(0, 255, 0),
-		CV_RGB(255, 0, 0),
-		CV_RGB(0, 0, 255),
-		CV_RGB(255, 255, 0),
-		CV_RGB(255, 0, 255),
-		CV_RGB(0, 255, 255)
-	};
+    cv::Scalar colorPreset[] = {
+        CV_RGB(0, 255, 0),
+        CV_RGB(255, 0, 0),
+        CV_RGB(0, 0, 255),
+        CV_RGB(255, 255, 0),
+        CV_RGB(255, 0, 255),
+        CV_RGB(0, 255, 255)
+    };
 
-	cv::SIFT sift = cv::SIFT();
-	vector<cv::KeyPoint> keypointsA, keypointsB;
-	cv::Mat descA, descB;
-	cv::Mat imgA, imgB;
+    cv::SIFT sift = cv::SIFT();
+    vector<cv::KeyPoint> keypointsA, keypointsB;
+    cv::Mat descA, descB;
+    cv::Mat imgA, imgB;
 
-	cv::cvtColor(this->prevFrame, imgA, CV_BGR2GRAY);
-	cv::cvtColor(this->nextFrame, imgB, CV_BGR2GRAY);
+    cv::cvtColor(this->prevFrame, imgA, CV_BGR2GRAY);
+    cv::cvtColor(this->nextFrame, imgB, CV_BGR2GRAY);
 
-	sift(imgA, cv::Mat(), keypointsA, descA);
-	sift(imgB, cv::Mat(), keypointsB, descB);
+    sift(imgA, cv::Mat(), keypointsA, descA);
+    sift(imgB, cv::Mat(), keypointsB, descB);
 
-	cv::Ptr<cv::DescriptorMatcher> matcher =
-		cv::DescriptorMatcher::create("BruteForce");
+    cv::Ptr<cv::DescriptorMatcher> matcher =
+        cv::DescriptorMatcher::create("BruteForce");
 
-	vector<cv::Mat> matchMasks; // match masks for each prevCandidates
-	vector<int> cnt;
-	cv::Mat matchMask;
+    vector<cv::Mat> matchMasks; // match masks for each prevCandidates
+    vector<int> cnt;
+    cv::Mat matchMask;
 
-	printf("prevCddsSize is %d", prevCddsSize);
+    printf("prevCddsSize is %d", prevCddsSize);
 
-	for (int i = 0; i < prevCddsSize; ++i) {
-		// matchMask initialize
-		matchMask = cv::Mat::zeros(keypointsA.size(), keypointsB.size(), CV_8UC1);
-		matchMasks.push_back(matchMask.clone());
-		cnt.push_back(0);
-	}
+    for (int i = 0; i < prevCddsSize; ++i) {
+        // matchMask initialize
+        matchMask = cv::Mat::zeros(keypointsA.size(), keypointsB.size(), CV_8UC1);
+        matchMasks.push_back(matchMask.clone());
+        cnt.push_back(0);
+    }
 
-	int cnt_k = 0; // cnt for keypoints
+    int cnt_k = 0; // cnt for keypoints
 
-	// classify each rect's feature with making match mask
-	for (vector<cv::KeyPoint>::const_iterator it = keypointsA.cbegin();
-		it != keypointsA.cend();
-		++it, ++cnt_k){
-		const cv::KeyPoint& kpA = keypointsA[cnt_k];
-		int j;
-		for (int i = 0; i < prevCddsSize; ++i) {
-			if (prevCandidates[i]->rect.contains(kpA.pt)) {
-				for (j = 0; j < keypointsB.size(); j++){
-					matchMasks[i].at<uchar>(cnt_k, j) = 1;
-					cnt[i]++;
-				}
-			}
-			else{
-				for (j = 0; j < keypointsB.size(); j++){
-					matchMasks[i].at<uchar>(cnt_k, j) = 0;
-				}
-			}
-		}
-	}
-	
-	// drawing the results
-	cv::Mat img_matches;
-	
-	vector< vector<cv::DMatch> > matches_list; // vector for all matches from each prev candidates
+    // classify each rect's feature with making match mask
+    for (vector<cv::KeyPoint>::const_iterator it = keypointsA.cbegin();
+        it != keypointsA.cend();
+        ++it, ++cnt_k){
+        const cv::KeyPoint& kpA = keypointsA[cnt_k];
+        int j;
+        for (int i = 0; i < prevCddsSize; ++i) {
+            if (prevCandidates[i]->rect.contains(kpA.pt)) {
+                for (j = 0; j < keypointsB.size(); j++){
+                    matchMasks[i].at<uchar>(cnt_k, j) = 1;
+                    cnt[i]++;
+                }
+            }
+            else{
+                for (j = 0; j < keypointsB.size(); j++){
+                    matchMasks[i].at<uchar>(cnt_k, j) = 0;
+                }
+            }
+        }
+    }
 
-	cv::Mat next = nextFrame.clone();
+    // drawing the results
+    cv::Mat img_matches;
 
-	// find max rect from each prev candidates
-	for (int i = 0; i < prevCddsSize; ++i) {
-		vector<cv::DMatch> matches;
-		matches_list.push_back(matches);
+    vector< vector<cv::DMatch> > matches_list; // vector for all matches from each prev candidates
 
-		// split each prevCandidates' matches with match mask
-		matcher->match(descA, descB, matches, matchMasks[i]);
-	
-		vector< vector<cv::Rect> > eachRectCandidates; // vector for all rect candidates
-		vector<cv::Rect> rectCandidates; // vector for 10 rect candidates from each prev Candidates
+    cv::Mat next = nextFrame.clone();
 
-		srand(time(NULL));
-		for (int cnt_it = 0;
+    // find max rect from each prev candidates
+    for (int i = 0; i < prevCddsSize; ++i) {
+        vector<cv::DMatch> matches;
+        matches_list.push_back(matches);
+
+        // split each prevCandidates' matches with match mask
+        matcher->match(descA, descB, matches, matchMasks[i]);
+
+        vector< vector<cv::Rect> > eachRectCandidates; // vector for all rect candidates
+        vector<cv::Rect> rectCandidates; // vector for 10 rect candidates from each prev Candidates
+
+        srand(time(NULL));
+        for (int cnt_it = 0;
              cnt_it < UGPROJ_ASSOCIATOR_SIFT_TRIAL_COUNT;
              cnt_it++) {
 
-			// pick random match pointer
-			int idx_m1, idx_m2, idx_bp1, idx_bp2, idx_ap1, idx_ap2;
+            // pick random match pointer
+            int idx_m1, idx_m2, idx_bp1, idx_bp2, idx_ap1, idx_ap2;
 
-			idx_m1 = rand() % matches.size();
-			idx_m2 = rand() % matches.size();
+            idx_m1 = rand() % matches.size();
+            idx_m2 = rand() % matches.size();
 
-			idx_bp1 = matches[idx_m1].queryIdx;
-			idx_bp2 = matches[idx_m2].queryIdx;
-			idx_ap1 = matches[idx_m1].trainIdx;
-			idx_ap2 = matches[idx_m2].trainIdx;
+            idx_bp1 = matches[idx_m1].queryIdx;
+            idx_bp2 = matches[idx_m2].queryIdx;
+            idx_ap1 = matches[idx_m1].trainIdx;
+            idx_ap2 = matches[idx_m2].trainIdx;
 
-			int x1, y1, x2, y2, ax1, ay1, ax2, ay2;
+            int x1, y1, x2, y2, ax1, ay1, ax2, ay2;
 
-			x1 = keypointsA[idx_bp1].pt.x;
-			y1 = keypointsA[idx_bp1].pt.y;
-			x2 = keypointsA[idx_bp2].pt.x;
-			y2 = keypointsA[idx_bp2].pt.y;
+            x1 = keypointsA[idx_bp1].pt.x;
+            y1 = keypointsA[idx_bp1].pt.y;
+            x2 = keypointsA[idx_bp2].pt.x;
+            y2 = keypointsA[idx_bp2].pt.y;
 
-			ax1 = keypointsB[idx_ap1].pt.x;
-			ay1 = keypointsB[idx_ap1].pt.y;
-			ax2 = keypointsB[idx_ap2].pt.x;
-			ay2 = keypointsB[idx_ap2].pt.y;
+            ax1 = keypointsB[idx_ap1].pt.x;
+            ay1 = keypointsB[idx_ap1].pt.y;
+            ax2 = keypointsB[idx_ap2].pt.x;
+            ay2 = keypointsB[idx_ap2].pt.y;
 
-			// calculate pseudo inverse or inverse
+            // calculate pseudo inverse or inverse
             Eigen::MatrixXd matA(4, 3);
             Eigen::VectorXd matB(4);
             Eigen::VectorXd matX;
 
-			matA << x1, 1, 0,
-				    x2, 1, 0,
-				    y1, 0, 1,
-				    y2, 0, 1;
-			matB << ax1, ax2, ay1, ay2;
+            matA << x1, 1, 0,
+                    x2, 1, 0,
+                    y1, 0, 1,
+                    y2, 0, 1;
+            matB << ax1, ax2, ay1, ay2;
 
-			findSAB(matA, matB, matX);
+            findSAB(matA, matB, matX);
 
-			cv::Rect calculatedRect;
-			calculatedRect = prevCandidates[i]->rect;
-			int bef_x1 = calculatedRect.x;
-			int bef_y1 = calculatedRect.y;
-			int bef_x2 = calculatedRect.x + calculatedRect.width - 1;
-			int bef_y2 = calculatedRect.y + calculatedRect.height - 1;
+            cv::Rect calculatedRect;
+            calculatedRect = prevCandidates[i]->rect;
+            int bef_x1 = calculatedRect.x;
+            int bef_y1 = calculatedRect.y;
+            int bef_x2 = calculatedRect.x + calculatedRect.width - 1;
+            int bef_y2 = calculatedRect.y + calculatedRect.height - 1;
 
-			// when pseudo inverse
-			int aft_x1 = (int)(matX[0] * (double)bef_x1 + matX[1]);
-			int aft_y1 = (int)(matX[0] * (double)bef_y1 + matX[2]);
-			int aft_x2 = (int)(matX[0] * (double)bef_x2 + matX[1]);
-			int aft_y2 = (int)(matX[0] * (double)bef_y2 + matX[2]);
+            // when pseudo inverse
+            int aft_x1 = (int)(matX[0] * (double)bef_x1 + matX[1]);
+            int aft_y1 = (int)(matX[0] * (double)bef_y1 + matX[2]);
+            int aft_x2 = (int)(matX[0] * (double)bef_x2 + matX[1]);
+            int aft_y2 = (int)(matX[0] * (double)bef_y2 + matX[2]);
 
-			// cut boundry
+            // cut boundry
             aft_x1 = std::min( std::max(0, aft_x1), imgA.size().width - 1 );
             aft_y1 = std::min( std::max(0, aft_y1), imgA.size().height - 1 );
             aft_x2 = std::min( std::max(0, aft_x2), imgA.size().width - 1 );
             aft_y2 = std::min( std::max(0, aft_y2), imgA.size().height - 1 );
 
-			calculatedRect.x = aft_x1;
-			calculatedRect.y = aft_y1;
-			calculatedRect.width = aft_x2 - aft_x1;
-			calculatedRect.height = aft_y2 - aft_y1;
+            calculatedRect.x = aft_x1;
+            calculatedRect.y = aft_y1;
+            calculatedRect.width = aft_x2 - aft_x1;
+            calculatedRect.height = aft_y2 - aft_y1;
 
-			rectCandidates.push_back(calculatedRect);
+            rectCandidates.push_back(calculatedRect);
 
-		}
-		eachRectCandidates.push_back(rectCandidates);
+        }
+        eachRectCandidates.push_back(rectCandidates);
 
-		// count which is included to each rect Candidates among current prevCandidate's match keypointsB
-		printf("\ncalculate inlier ratio\n");
-		vector<int> cnt_match(UGPROJ_ASSOCIATOR_SIFT_TRIAL_COUNT, 0); // matched feature with each prev Candidate
-		vector<int> cnt_all(UGPROJ_ASSOCIATOR_SIFT_TRIAL_COUNT, 0); // all matched feature
+        // count which is included to each rect Candidates among current prevCandidate's match keypointsB
+        printf("\ncalculate inlier ratio\n");
+        vector<int> cnt_match(UGPROJ_ASSOCIATOR_SIFT_TRIAL_COUNT, 0); // matched feature with each prev Candidate
+        vector<int> cnt_all(UGPROJ_ASSOCIATOR_SIFT_TRIAL_COUNT, 0); // all matched feature
 
-		// calculate cnt_all
+        // calculate cnt_all
         vector<cv::KeyPoint>::const_iterator itKeypB;
         for (itKeypB = keypointsB.cbegin(); itKeypB != keypointsB.cend(); ++itKeypB) {
-			const cv::KeyPoint& target = *itKeypB;
-			for (int cnt_rc = 0;
+            const cv::KeyPoint& target = *itKeypB;
+            for (int cnt_rc = 0;
                  cnt_rc < UGPROJ_ASSOCIATOR_SIFT_TRIAL_COUNT;
                  ++cnt_rc) {
-				if (rectCandidates[cnt_rc].contains(target.pt)) {
-					cnt_all[cnt_rc]++;
-				}
-			}
-		}
+                if (rectCandidates[cnt_rc].contains(target.pt)) {
+                    cnt_all[cnt_rc]++;
+                }
+            }
+        }
 
-		// calculate cnt_match
-		for (vector<cv::DMatch>::const_iterator it = matches.cbegin();
-			it != matches.cend();
-			++it){
+        // calculate cnt_match
+        for (vector<cv::DMatch>::const_iterator it = matches.cbegin();
+            it != matches.cend();
+            ++it){
             const cv::DMatch& match = *it;
-			const cv::KeyPoint& target = keypointsB[match.trainIdx];
-			for (int cnt_rc = 0;
+            const cv::KeyPoint& target = keypointsB[match.trainIdx];
+            for (int cnt_rc = 0;
                  cnt_rc < UGPROJ_ASSOCIATOR_SIFT_TRIAL_COUNT;
                  ++cnt_rc) {
-				if (rectCandidates[cnt_rc].contains(target.pt)) {
-					cnt_match[cnt_rc]++;
-				}
-			}
-		}
+                if (rectCandidates[cnt_rc].contains(target.pt)) {
+                    cnt_match[cnt_rc]++;
+                }
+            }
+        }
 
-		// find max ratio box
-		cv::Rect maxRect;
-		double max = 0;
-		printf("find Max Ratio Box\n");
-		for (int cnt_rc = 0;
+        // find max ratio box
+        cv::Rect maxRect;
+        double max = 0;
+        printf("find Max Ratio Box\n");
+        for (int cnt_rc = 0;
              cnt_rc < UGPROJ_ASSOCIATOR_SIFT_TRIAL_COUNT;
              ++cnt_rc) {
             double inlierRatio = (double) cnt_match[cnt_rc] /
                                  (double) cnt_all[cnt_rc];
-			if (inlierRatio > max) {
-				max = inlierRatio;
-				maxRect = rectCandidates[cnt_rc];
-			}
-		}
+            if (inlierRatio > max) {
+                max = inlierRatio;
+                maxRect = rectCandidates[cnt_rc];
+            }
+        }
 
-		printf("Max Rect (%d,%d) - (%d,%d)\n", maxRect.x, maxRect.y, maxRect.x + maxRect.width - 1, maxRect.y + maxRect.height);
-		rectangle(next,
-			cvPoint(maxRect.x, maxRect.y),
-			cvPoint(maxRect.x + maxRect.width - 1, maxRect.y + maxRect.height),
-			colorPreset[i]);
+        printf("Max Rect (%d,%d) - (%d,%d)\n", maxRect.x, maxRect.y, maxRect.x + maxRect.width - 1, maxRect.y + maxRect.height);
+        rectangle(next,
+            cvPoint(maxRect.x, maxRect.y),
+            cvPoint(maxRect.x + maxRect.width - 1, maxRect.y + maxRect.height),
+            colorPreset[i]);
 
-		// draw matches
-		drawMatches(imgA, keypointsA, next, keypointsB, matches, img_matches, colorPreset[i], cv::Scalar::all(-1), vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+        // draw matches
+        drawMatches(imgA, keypointsA, next, keypointsB, matches, img_matches, colorPreset[i], cv::Scalar::all(-1), vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
-	}
+    }
 
-	char filename[100];
-	sprintf(filename, "output/result%d.jpg", result_cnt++);
-	imwrite(filename, img_matches);
+    char filename[100];
+    sprintf(filename, "output/result%d.jpg", result_cnt++);
+    imwrite(filename, img_matches);
 
 }
 
