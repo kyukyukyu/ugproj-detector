b7ae739407d892b1a55e2eb4e2a7d6601822b82b
Sanggyu Nam <pokeplus@gmail.com>
2015-07-21 22:53:47 +0900

    Add video writing function to FileWriter
    * For issue #16.

--- src/file_io.h
+++ src/file_io.h
@@ -5,6 +5,7 @@
 #include "structure.hpp"
 
 #include <string>
+#include <unordered_map>
 #include <boost/filesystem.hpp>
 #include <opencv2/opencv.hpp>
 
@@ -25,9 +26,20 @@ class FileWriter {
   public:
     int init(const Arguments& args);
     int write_image(const cv::Mat& image, const std::string& filename) const;
+    // Opens a video file to write with given key string and properties.
+    // Returns non-zero value if the file is not opened correctly, otherwise 0.
+    int open_video_file(
+        const std::string& key,
+        const std::string& filename,
+        const double fps,
+        const cv::Size& frame_size);
+    // Writes a new frame into video file with given key string. Returns
+    // non-zero value if writing was not successful, otherwise 0.
+    int write_video_frame(const cv::Mat& frame, const std::string& key);
 
   private:
     boost::filesystem::path output_path_;
+    std::unordered_map<std::string, cv::VideoWriter> video_files_;
 };
 
 }  // namespace ugproj
